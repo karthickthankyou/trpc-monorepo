a4cfd9d532370a0d3ea7e1b1c8dd448bf97a57ce
Karthick Ragavendran <karthickthankyou@gmail.com>
2024-01-30 07:08:32 +0530

    create home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,22 +1,9 @@
-'use client'
-import { getAuth } from '@foundation-trpc/network/src/auth/authOptions'
-import { trpcClient } from '@foundation-trpc/trpc-client/src/client'
-import { signOut, useSession } from 'next-auth/react'
-import Link from 'next/link'
+import { HomePage } from '@foundation-trpc/ui/src/components/templates/HomePage'
 
 export default function Home() {
-  const { data } = trpcClient.auth.users.useQuery()
-  const { data: userData } = useSession()
   return (
     <main>
-      Hello {JSON.stringify(data)}
-      <div>
-        {userData?.user ? (
-          <button onClick={() => signOut()}>signout</button>
-        ) : (
-          <Link href="/signIn">Sign in</Link>
-        )}
-      </div>
+      <HomePage />
     </main>
   )
 }
---
+++ libs/ui/src/components/templates/HomePage.tsx
@@ -0,0 +1,87 @@
+import Link from 'next/link'
+import React from 'react'
+import { GitCommit } from 'lucide-react'
+
+export const HomePage = () => {
+  return (
+    <div className="max-w-2xl my-8 space-y-8">
+      <h1 className="text-3xl font-bold mb-4">
+        Monorepo for Web Application Development
+      </h1>
+
+      <div>
+        <Link
+          href="https://github.com/karthickthankyou/foundation-trpc"
+          target="_blank"
+          className="inline-flex gap-2 border px-2 py-1"
+        >
+          <GitCommit /> Github <GitCommit />
+        </Link>
+      </div>
+      <h2 className="text-xl font-semibold mb-2">Structure</h2>
+
+      <div>
+        <h3 className="text-lg font-semibold mb-2">Apps</h3>
+        <ul className="list-disc ml-6 space-y-2">
+          <li>web: Next.js application for the frontend user interface.</li>
+          <li>
+            api: Express backend API powered by TRPC for efficient remote
+            procedure calls.
+          </li>
+        </ul>
+      </div>
+
+      <div className=" mt-4">
+        <h3 className="text-lg font-semibold mb-2">Libs</h3>
+        <ul className="list-disc ml-6 space-y-2">
+          <li>
+            db: Database interactions handled by Prisma, ensuring data
+            consistency and efficiency.
+          </li>
+          <li>
+            forms: Secure and reliable form validation and management with Zod
+            and React Hook Form.
+          </li>
+          <li>
+            network: NextAuth handles authentication and network-related logic
+            with ease.
+          </li>
+          <li>
+            trpc-client: Configuration for the frontend to seamlessly
+            communicate with the TRPC server. It supports both the client and
+            react server components.
+          </li>
+          <li>trpc-server: Robust TRPC server setup for the backend API.</li>
+          <li>
+            ui: Reusable and encapsulated UI components built with Shadow DOM
+            and styled with Tailwind CSS.
+          </li>
+        </ul>
+      </div>
+
+      <h2 className="text-xl font-semibold my-4">Key Features</h2>
+      <ul className="list-disc ml-6 space-y-2">
+        <li>
+          TypeScript: Enforces strong typing for a clean, maintainable, and
+          error-free codebase.
+        </li>
+        <li>
+          Static Testing: Maintain code quality and catch potential issues early
+          with Prettier, ESLint, and TypeScript.
+        </li>
+        <li>
+          Type Propagation: Database types seamlessly flow to UI components,
+          ensuring type safety and preventing errors.
+        </li>
+        <li>
+          Modern Tooling: Leverage the power of Prisma, TRPC, and NextAuth for
+          efficient data management, communication, and authentication.
+        </li>
+        <li>
+          Reusable Components: Build scalable and maintainable UIs with shared
+          components, Shadow DOM encapsulation, and Tailwind CSS styling.
+        </li>
+      </ul>
+    </div>
+  )
+}
