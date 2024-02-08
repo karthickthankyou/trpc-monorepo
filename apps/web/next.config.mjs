fbb62f5840ed8cee73dedadc8131b5e2bb1fdd1d
Karthick Ragavendran <karthickthankyou@gmail.com>
2024-02-08 13:14:34 +0530

    add images in next config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'lh3.googleusercontent.com' },
+      { hostname: 'firebasestorage.googleapis.com' },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
